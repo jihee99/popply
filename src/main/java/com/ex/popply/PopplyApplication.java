d022275e0ba041babdf2ff1f90e6bfc9658388eb
jihee99 <oosoojh31@gmail.com>
2024-07-29 18:44:20 +0900

    init

---
+++ src/main/java/com/ex/popply/PopplyApplication.java
@@ -0,0 +1,13 @@
+package com.ex.popply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ex/popply/PopplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.popply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
